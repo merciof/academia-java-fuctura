88fd2466e2183a661edfa30e7cceb7a87fee9fa9
JAVA <JAVA@192.168.25.90>
2019-06-15 13:56:43 -0300

    Adição de atributo id_pessoa.

--- JavaApplication5/src/entidades/Pessoa.java
+++ JavaApplication5/src/entidades/Pessoa.java
@@ -10,11 +10,21 @@ package entidades;
  * @author JAVA
  */
 public class Pessoa {
+    private int id_pessoa;
     private String nome;
     private String cpf;
     private String idade;
     private String sexo;
     private Endereco endereco;
+    private Conta conta;
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
 
     public Pessoa(String nome, String cpf, String idade, String sexo, Endereco endereco) {
         this.nome = nome;
@@ -26,8 +36,16 @@ public class Pessoa {
 
     public Pessoa() {
     }
-    
 
+    public int getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+    
+    
     public String getNome() {
         return nome;
     }
