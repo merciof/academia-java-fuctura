26dc7722e64d5222bf5b598691f8c97d825001f4
merciof <mercio.andrade@ufrpe.br>
2019-06-26 19:44:01 -0300

    Adição de try catch para tratamento da exceção SQLException.

---
+++ JavaApplication5/src/util/Conexao.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oracle.jdbc.pool.OracleDataSource;
+
+/**
+ *
+ * @author JAVA
+ */
+public class Conexao {
+    
+    private final String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+    private final String user = "mercio";
+    private final String password = "admin";
+    private Connection connection;
+    
+    public Connection getConnection() {
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+//            connection = DriverManager.getConnection(jdbcUrl, user, password);
+//            connection.close();
+
+        try {
+            
+            OracleDataSource ds;
+            ds = new OracleDataSource();
+            ds.setURL(jdbcUrl);
+            connection = ds.getConnection(user, password);
+     
+        } catch (SQLException e) {
+            
+        }
+
+        System.out.println("banco conectado");
+        return connection;
+
+    }
+    
+    public void closeConnection(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            System.out.println( "Erro ao fechar conexão com o bd: " + ex.getMessage());
+        }
+    }
+}
