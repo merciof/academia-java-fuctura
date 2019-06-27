2ad297e681b1777261e485f9f39f9f4e91583db7
merciof <mercio.andrade@ufrpe.br>
2019-06-26 22:21:04 -0300

    Adição do método 'selecionar' a classe EnderecoDAO.

--- JavaApplication5/src/dao/EnderecoDAO.java
+++ JavaApplication5/src/dao/EnderecoDAO.java
@@ -23,18 +23,21 @@ public class EnderecoDAO {
     Connection connection = conexao.getConnection();
     
     public void inserir(Endereco endereco) {
-        String sql = "INSERT INTO ENDERECO (RUA,NUMERO,COMPLEMENTO) VALUES(?,?,?)";
+        
+        String sql = "INSERT INTO ENDERECO (RUA,NUMERO,CIDADE,ESTADO) VALUES(?,?,?,?)";
         
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1, endereco.getLogradouro());
             ps.setInt(2, endereco.getNumero());
             ps.setString(3, endereco.getCidade());
+            ps.setString(4, endereco.getEstado());
+            //ps.setInt(5, endereco.getId_endereco());
             ps.execute();
             System.out.println("Inserido com sucesso");
             
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Erro na inserção de endereço " + e.getMessage());
         }  finally {
             conexao.closeConnection(connection);
         } 
@@ -68,27 +71,48 @@ public class EnderecoDAO {
         }  
     }
     
-    public void selecionar(int idEndereco) {
+    public Endereco selecionar(int idEndereco) {
+        
         String sql = "SELECT * FROM ENDERECO WHERE ID_ENDERECO = ?";
+        
+        Endereco endereco = new Endereco();
         
         try {
             
+            PreparedStatement ps = connection.prepareStatement(sql);  
+            ps.setInt(1, idEndereco);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            endereco.setCidade(rs.getString("CIDADE"));
+            endereco.setLogradouro(rs.getString("RUA"));
+            endereco.setNumero(rs.getInt("NUMERO"));
+            endereco.setEstado(rs.getString("ESTADO"));
+            endereco.setId_endereco(rs.getInt("ID_ENDERECO"));
+
         } catch (Exception e) {
+            System.out.println("Erro ao selecionar endereco: " + e.getMessage());
         }
+        
+        return endereco;
     }
     
-    public List<Endereco> listarEndereco() {
+    public List<Endereco> listarEndereco() {       
+        
         String sql = "SELECT * FROM ENDERECO";
+        
         List<Endereco> retorno = new ArrayList<Endereco>();
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
+        
+        try {        
+            PreparedStatement ps = connection.prepareStatement(sql);         
             ResultSet rs = ps.executeQuery();
             
-            while(rs.next()) {
+            while(rs.next()) {             
                 Endereco endereco = new Endereco();
-                endereco.setCidade(rs.getString("COMPLEMENTO"));
-                endereco.setLogradouro(rs.getString("ID_ENDERECO"));
+                endereco.setCidade(rs.getString("CIDADE"));
+                endereco.setLogradouro(rs.getString("LOGRADOURO"));
                 endereco.setNumero(rs.getInt("NUMERO"));
+                endereco.setEstado(rs.getString("ESTADO"));
+                endereco.setId_endereco(rs.getInt("ID"));
                 retorno.add(endereco);    
             } 
             
