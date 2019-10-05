2b2d9be15870f32e129a6f86cdb73e3dddc7e3fb
JAVA <JAVA@ES01-MAQ08>
2019-10-05 12:09:36 -0300

    ligacao de tela manterPergunta com o perguntaBean

--- JavaMonster_2/src/main/java/controle/PerguntaBean.java
+++ JavaMonster_2/src/main/java/controle/PerguntaBean.java
@@ -1,5 +1,63 @@
 package controle;
+import dao.PerguntaDAO;
+import dao.PerguntaDAOImpl;
+import dao.UsuarioDAO;
+import dao.UsuarioDAOImpl;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import entidades.Pergunta;
+import entidades.Resposta;
+import entidades.Usuario;
+import util.JpaUtil;
+
+@ManagedBean(name="PerguntaBean")
+@RequestScoped
 public class PerguntaBean {
+	
+	Pergunta pergunta;
+	List<Usuario> listUsuario;
+	
+	PerguntaDAO perguntaDAO;
+	UsuarioDAO usuarioDAO;
+	
+	
+	public PerguntaBean() {
+		this.perguntaDAO = new PerguntaDAOImpl(JpaUtil.getEntityManager());
+		this.usuarioDAO = new UsuarioDAOImpl(JpaUtil.getEntityManager());
+		this.pergunta = new Pergunta();
+		
+		this.pergunta.setUsuario(new Usuario());
+		
+		this.listUsuario = this.usuarioDAO.listarTodos();
+	}
+	
+	public void salvar() {
+		
+	}
 
+	public Pergunta getPergunta() {
+		return pergunta;
+	}
+
+	public void setPergunta(Pergunta pergunta) {
+		this.pergunta = pergunta;
+	}
+
+	public List<Usuario> getListUsuario() {
+		return listUsuario;
+	}
+
+	public void setListUsuario(List<Usuario> listUsuario) {
+		this.listUsuario = listUsuario;
+	}
+	
+	
+	
+	
+	
 }
